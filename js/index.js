a39badd808802b033fb17b6693db0034d55f50df
fayekafateh <111707631+fayekafateh@users.noreply.github.com>
2023-01-27 18:29:56 -0500

    HW 20

--- js/index.js
+++ js/index.js
@@ -2,14 +2,19 @@
 $(".gtg").hover(function(){
     $(".gtgbox").css("transform","translateX(100px)")
     $(".h1gtg").css("transform","translateX(-100px)")
-    $(".gtgButton").fadeIn(3000);
+    $(".gtgButton").fadeIn(3000)
+    $(".gtgimage1").fadeIn(3000)
+    $(".gtgimage2").fadeIn(3000);
 
 })
 
 $(".dAssist").hover(function(){
     $(".dAssistbox").css("transform","translateX(-100px)")
     $(".h1dAssist").css("transform","translateX(100px)")
-    $(".dAssistButton").fadeIn(3000);
+    $(".dAssistButton").fadeIn(3000)
+    $(".dAssistimage1").fadeIn(3000)
+    $(".dAssistimage2").fadeIn(3000)
+    $(".dAssistimage3").fadeIn(3000);
 
 })
 
