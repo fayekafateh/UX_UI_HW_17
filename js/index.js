b94ebf338b30c0837c3f20c8eb31a4dfc4df0641
fayekafateh <111707631+fayekafateh@users.noreply.github.com>
2023-01-30 18:10:44 -0500

    HW 20

--- js/index.js
+++ js/index.js
@@ -21,6 +21,34 @@ $(".dAssist").hover(function(){
 $(".mFerals").hover(function(){
     $(".mFeralsbox").css("transform","translateX(100px)")
     $(".h1mFerals").css("transform","translateX(-100px)")
-    $(".mFeralsButton").fadeIn(3000);
+    $(".mFeralsButton").fadeIn(3000)
+    $(".mFeralsimage1").fadeIn(3000)
+    $(".mFeralsgif1").fadeIn(3000)
+    $(".mFeralsgif2").fadeIn(3000);
 
 })
+
+// Nav-Buttons
+$(".buttonTop").hover(function(){
+    $(".icontagstop").fadeIn(500);},function(){$(".icontagstop").fadeOut(100);}
+);
+
+$(".buttonPort").hover(function(){
+    $(".icontagsPort").fadeIn(500);},function(){$(".icontagsPort").fadeOut(100);}
+);
+
+$(".buttonExp").hover(function(){
+    $(".icontagsexp").fadeIn(500);},function(){$(".icontagsexp").fadeOut(100);}
+);
+
+$(".buttonSkl").hover(function(){
+    $(".icontagsSkl").fadeIn(500);},function(){$(".icontagsSkl").fadeOut(100);}
+);
+
+$(".buttonAbt").hover(function(){
+    $(".icontagsabt").fadeIn(500);},function(){$(".icontagsabt").fadeOut(100);}
+);
+
+$(".buttonCon").hover(function(){
+    $(".icontagscon").fadeIn(500);},function(){$(".icontagscon").fadeOut(100);}
+);
